5b64100096e7c9f242e6f75ee677b65738c8fe7e
GepetoinTraining <garcia.pedro.wow@gmail.com>
2025-10-28 00:54:45 -0300

    updated exports in prisma.ts

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -1,5 +1,5 @@
 // apps/web/app/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, ItemStatus } from '@prisma/client'; // <-- Import ItemStatus here
 
 // PrismaClient is attached to the `global` object in development
 // to prevent exhausting your database connection limit.
@@ -10,3 +10,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+// Re-export the enum so other files can import it from here
+export { ItemStatus };
+
